9a8a48cf9ac743f455d12a88a5cc85858a9ae359
Facundo Martin <facundomartin98@gmail.com>
2022-01-19 10:07:10 -0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ const navMenu = document.getElementById('nav-menu'),
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
-    })
+    }) 
 }
 
 /*===== MENU HIDDEN =====*/
